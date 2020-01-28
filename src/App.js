80f7a61bc5165d4347613251073e542cd4376209
Allegra Anka <allegra.anka@gmail.com>
2020-01-27 21:41:24 -0500

    search box component is functional and reusable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import './App.css';
+import { SearchBox} from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
@@ -19,16 +20,16 @@ class App extends Component {
   }
 
   render () {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return(
       <div className='App'>
-        <input 
-          type='search' 
+        <SearchBox 
           placeholder='Search Monsters' 
-          onChange={e => {
-            this.setState({ searchField: e.target.value });
-          }}
-        />
-        <CardList monsters={this.state.monsters} />
+          handleChange={e => this.setState({ searchField: e.target.value })}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
