d9e606f705e3d343cb8990a4716002f725840e64
Allegra Megan Anka <allegra.anka@gmail.com>
2020-05-03 19:10:38 -0400

    Updates

---
+++ precache-manifest.3294b7413c60cc62df834289f85124ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752b771ad1d74f0a517f5934a5a7e8b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f17565a3f0481ec4f175",
+    "url": "/monsters-rolodex/static/css/main.b08b0af8.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f17565a3f0481ec4f175",
+    "url": "/monsters-rolodex/static/js/main.56ecbe7c.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
